818be18c704d7691640fd1d236f3a56c17f833a3
Luke Engelhardt <luke@securitymetrics.com>
2015-07-06 08:37:14 -0600

    corrected 1 off error

--- closures.js
+++ closures.js
@@ -87,14 +87,13 @@ console.log(func(function(){return 'Only executed once'})());
 
 
 var fnCounter = function (anonFunc, N) {
-    for (var i = 0; i <= N; i++) {
+    for (var i = 0; i < N; i++) {
         anonFunc();
-        };
-    }
+        }
     return 'STOP';
 };
 
-console.log(fnCounter(function(){return 'this will run 3 times'}, 3));
+fnCounter(function(){console.log('this will run 3 times')}, 3);
 
 /*
   var counter = function(){
@@ -105,9 +104,9 @@ console.log(fnCounter(function(){return 'this will run 3 times'}, 3));
     }
   };
 
-Above you have a function named counter. Examine the function (without running the code) then below write what you expect to happen when the funciton is invoked. *Hint: setTimeout calls a function or evaluates an expression after a specified number of milliseconds.
+Above you have a function named counter. Examine the function (without running the code) then below write what you expect to happen when the function is invoked. *Hint: setTimeout calls a function or evaluates an expression after a specified number of milliseconds.
 
-    //Answer Here  --  Nothing will happen.  There is a scoping problem with the variable i.  i is not accessible to the console.log call since it was declared in a function above.
+    //Answer Here  -- I think you have to pass i to the setTimeout function so it looks at the value of i BEFORE waiting, not after.  Waiting after i will probably always be 6.
 
 
 
